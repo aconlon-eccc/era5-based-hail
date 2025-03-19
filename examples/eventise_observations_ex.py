51b11899c858549ed30f9db0fa4730bf47bf1b50
aconlon-eccc <144841301+aconlon-eccc@users.noreply.github.com>
2025-03-19 09:15:59 -0400

    Update eventise_observations_ex.py

--- examples/eventise_observations_ex.py
+++ examples/eventise_observations_ex.py
@@ -5,7 +5,7 @@ Eventise hail observation data
 
 Use 'ERA5_Based_Hail.data_processing.eventise_data.observations'.
 
-We have a csv file of 7000 hail reports from all over Canada between 2005 and 2022 called 'hail_db_with_LD.csv'.
+We have a csv file of 7000 hail reports from all over Canada between 2005 and 2022 called 'integrated_canadian_hail_db.csv'.
 
 Some of these reports are from the same hail event, i.e. a single hail storm produces multiple reports from different
 locations throughout its life. So we would like to bundle reports into hail 'events' based on the time of the report
